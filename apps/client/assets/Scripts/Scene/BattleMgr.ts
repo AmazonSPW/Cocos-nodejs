7a0a9c2ff9965e7cfd56d52b3c08cb2067a783dd
AmazonSPW <778116154@qq.com>
2023-06-12 17:59:27 +0800

    feat: 增加地图

--- apps/client/assets/Scripts/Scene/BattleMgr.ts
+++ apps/client/assets/Scripts/Scene/BattleMgr.ts
@@ -2,11 +2,12 @@
  * @Author       : pengwei.shi
  * @Date         : 2023-06-11 22:02:37
  * @LastEditors  : pengwei.shi
- * @LastEditTime : 2023-06-12 17:39:47
+ * @LastEditTime : 2023-06-12 17:56:54
  * @FilePath     : \client\assets\Scripts\Scene\BattleMgr.ts
  * @Description  : 
  */
 import { _decorator, Component, instantiate, Node, Prefab } from 'cc';
+import { EEntityType } from '../Common';
 import { ActorMgr } from '../Entity/Actor/ActorMgr';
 import { EPrefabPath } from '../Enum';
 import DataManager from '../Global/DataManager';
@@ -28,7 +29,15 @@ export class BattleMgr extends Component {
     protected async start(): Promise<void> {
         this.stage.destroyAllChildren();
         await this.loadRes();
+        this.initMap();
         this._useUpdate = true;
+    }
+
+    protected initMap() {
+        let prefab = DataManager.Instance.prefabMap.get(EEntityType.Map);
+        if (!prefab) return;
+        let map = instantiate(prefab);
+        map.setParent(this.stage);
     }
 
     private async loadRes() {
