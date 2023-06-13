600c06e6474c5686677492feeca5eccd9f4e91a2
AmazonSPW <778116154@qq.com>
2023-06-13 17:07:41 +0800

    feat: 服务端

--- apps/server/src/Common/Enum.ts
+++ apps/server/src/Common/Enum.ts
@@ -2,11 +2,23 @@
  * @Author       : pengwei.shi
  * @Date         : 2023-06-11 22:33:52
  * @LastEditors  : pengwei.shi
- * @LastEditTime : 2023-06-11 22:33:53
- * @FilePath     : \cocos-nodejs-io-game-start-demo\apps\client\assets\Scripts\Common\Enum.ts
+ * @LastEditTime : 2023-06-13 16:49:45
+ * @FilePath     : \client\assets\Scripts\Common\Enum.ts
  * @Description  : 
  */
 
 export enum InputTypeEnum {
     ActorMove = "ActorMove",
+    WeaponShoot = "WeaponShoot",
 }
+
+/**
+ * 实体类型
+ */
+export enum EntityTypeEnum {
+    Actor1 = "Actor1",
+    Map = "Map",
+    Weapon1 = "Weapon1",
+    Bullet1 = "Bullet1",
+}
+
--- apps/server/src/Common/State.ts
+++ apps/server/src/Common/State.ts
@@ -2,12 +2,12 @@
  * @Author       : pengwei.shi
  * @Date         : 2023-06-11 22:07:35
  * @LastEditors  : pengwei.shi
- * @LastEditTime : 2023-06-11 22:37:01
- * @FilePath     : \cocos-nodejs-io-game-start-demo\apps\client\assets\Scripts\Common\State.ts
+ * @LastEditTime : 2023-06-13 16:51:10
+ * @FilePath     : \client\assets\Scripts\Common\State.ts
  * @Description  : 
  */
 
-import { InputTypeEnum } from "./Enum";
+import { EntityTypeEnum, InputTypeEnum } from "./Enum";
 
 
 export interface IVEC2 {
@@ -20,15 +20,39 @@ export interface IActor {
     id: number;
     position: IVEC2;
     direction: IVEC2;
+    type: EntityTypeEnum;
+    weaponType: EntityTypeEnum;
+    bulletType: EntityTypeEnum,
+}
+
+
+export interface IBullet {
+    id: number,
+    owner: number,
+    position: IVEC2,
+    direction: IVEC2,
+    type: EntityTypeEnum,
 }
 
 export interface IState {
-    actors: IActor[]
+    actors: IActor[],
+    bullets: IBullet[],
+    nextBulletID: number,
 }
+
+
+export type IClientInput = IActorMove | IWeaponShoot;
 
 export interface IActorMove {
     id: number,
-    type: InputTypeEnum,
+    type: InputTypeEnum.ActorMove,
     direction: IVEC2,
     dt: number,
+}
+
+export interface IWeaponShoot {
+    type: InputTypeEnum.WeaponShoot,
+    owner: number,
+    position: IVEC2,
+    direction: IVEC2,
 }
